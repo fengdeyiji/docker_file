80ce123dc99fa46feb4cabdceea55e4d2c0a7764
fengdeyiji <tengxuwang@gmail.com>
2024-01-05 10:41:23 +0800

    init vscode extensions when create docker

--- docker_file/build.sh
+++ docker_file/build.sh
@@ -3,6 +3,10 @@ for arg in "$@"; do
   if [ "$arg" = "prepare" ]; then
     sh ./clash/update_subscription.sh "boot"
     cat ~/.ssh/id_rsa.pub > authorized_keys #拷贝本物理机的公钥
+    if [ ! -f "llvm.tar.xz" ]; then
+      echo "llvm文件不存在, 开始下载"
+      wget -O llvm.tar.xz https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/LLVM%2017.0.6/clang%2Bllvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04.tar.xz
+    fi
   fi
   if [ "$arg" = "build" ]; then
     docker container ls -a | grep 'dev' | awk '{print $1}' | xargs docker container stop | xargs docker rm #如果有正在运行的docker，结束掉
@@ -10,7 +14,7 @@ for arg in "$@"; do
     docker build -t dev:v1 . #创建新的镜像
   fi
   if [ "$arg" = "run" ]; then
-    docker run -d --mount type=bind,source=/Users/txw/Desktop/src,target=/usr/share/src -p 1024:22 dev:v1
+    docker run -d --mount type=bind,source=/Users/txw/Desktop/src,target=/root/project -p 1024:22 dev:v1
     grep -v "\[127.0.0.1\]:1024" "$HOME/.ssh/known_hosts" > "$HOME/.ssh/known_hosts.tmp"
     mv "$HOME/.ssh/known_hosts.tmp" "$HOME/.ssh/known_hosts"
   fi
