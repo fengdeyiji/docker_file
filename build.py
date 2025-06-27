0392c37d6982304ef2e2c3078a7202d573de422f
xuwang.txw <xuwang.txw@gmail.com>
2025-06-27 18:55:27 +0800

    update

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
 import argparse
 import os
 import subprocess
-import re
 
-def DO(cmd : str):
-  print("CMD:{}".format(cmd))
-  result = subprocess.run(cmd, shell=True, stdout=None, stderr=None, encoding='utf-8')
-  assert(result.returncode == 0)
-  return result.stdout
-  
+
+def DO(cmd: str):
+    print("CMD:{}".format(cmd))
+    result = subprocess.run(cmd, shell=True, stdout=None,
+                            stderr=None, encoding='utf-8')
+    assert (result.returncode == 0)
+    return result.stdout
+
+
 parser = argparse.ArgumentParser(description='prepare/build/run docker')
 parser.add_argument('--prepare', default=False, action='store_true',
                     help='download necessary file from web')
@@ -24,19 +26,20 @@ args = parser.parse_args()
 
 os.chdir("./docker_file")
 if args.prepare:
-  if args.llvm_url is None or args.boost_url is None:
-    print(f"need url to download llvm and boost, see --help to get the right url")
+    if args.llvm_url is None or args.boost_url is None:
+    print("need url to download llvm and boost, see --help to get the right url")
     exit(0)
-  DO("rm -rf ./boost*")
-  DO("rm -rf ./*LLVM*")
-  DO(f"wget {args.llvm_url}")
-  DO(f"wget {args.boost_url}")
-  DO("cat ~/.ssh/id_rsa.pub > authorized_keys")
+    DO("rm -rf ./boost*")
+    DO("rm -rf ./*LLVM*")
+    DO(f"wget {args.llvm_url}")
+    DO(f"wget {args.boost_url}")
+    DO("cat ~/.ssh/id_rsa.pub > authorized_keys")
 if args.build:
-  DO("docker container ls -a | grep 'dev' | awk '{print $1}' | xargs -r docker container stop | xargs -r docker rm") #如果有正在运行的docker，结束掉
-  DO('''docker image ls | grep 'dev' | awk '{print $1":"$2}' | xargs -r docker image rm''') #删除已有的同名镜像
-  DO("docker build -t dev:v2 -f Dockerfile .") #创建新的镜像
+    # 如果有正在运行的docker，结束掉
+    DO("docker container ls -a | grep 'dev' | awk '{print $1}' | xargs -r docker container stop | xargs -r docker rm")
+    DO('''docker image ls | grep 'dev' | awk '{print $1":"$2}' | xargs -r docker image rm''')  # 删除已有的同名镜像
+    DO("docker build -t dev:v2 -f Dockerfile .")  # 创建新的镜像
 if args.run:
-  DO("docker run -d --mount type=bind,source=$HOME/Desktop/src,target=/root/project -p 1024:22 dev:v2")
-  DO('''grep -v "\[127.0.0.1\]:1024" "$HOME/.ssh/known_hosts" > "$HOME/.ssh/known_hosts.tmp"''')
-  DO('''mv "$HOME/.ssh/known_hosts.tmp" "$HOME/.ssh/known_hosts"''')
+    DO("docker run -d --mount type=bind,source=$HOME/Desktop/src,target=/home/admin/project -p 1024:22 dev:v2")
+    DO('''grep -v "\[127.0.0.1\]:1024" "$HOME/.ssh/known_hosts" > "$HOME/.ssh/known_hosts.tmp"''')
+    DO('''mv "$HOME/.ssh/known_hosts.tmp" "$HOME/.ssh/known_hosts"''')
--- docker_file/terminal/.zshrc
+++ docker_file/terminal/.zshrc
@@ -1,3 +1,4 @@
+typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -113,5 +114,3 @@ source $ZSH/oh-my-zsh.sh
 export PATH=/usr/local/llvm/bin:$PATH
 export PATH=~/.local/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-# core文件生成的设置
-ulimit -c unlimited
